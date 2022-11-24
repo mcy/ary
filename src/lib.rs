f8056b76bfdc72657b750be0bafa102607384195
Miguel Young de la Sota <mcyoung@mit.edu>
2022-11-24 15:27:35 -0500

    More license gaff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Copyright 2022 Miguel Young de la Sota
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! The array literal of the gods.
 //!
 //! Rust provides two syntaxes for array literals: `[a, b, c]`, and `[x; count]`.
